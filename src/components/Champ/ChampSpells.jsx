d9e09bdda618bc423f2f00b2422f480de5fecf82
thomas_lorthioir <tottilorthioir@gmail.com>
2022-09-08 16:18:19 -0400

    feat: (ChampSpells) design + letter for each spell

--- src/components/Champ/ChampSpells.jsx
+++ src/components/Champ/ChampSpells.jsx
@@ -3,6 +3,7 @@ import './ChampSpells.scss'
 const ChampSpells = (props) => {
     const champion = props.champion;
     const championCard = props.championCard
+    const spellsLetters = ['Q', 'W', 'E', 'R']
 
 
     return (
@@ -23,18 +24,26 @@ const ChampSpells = (props) => {
                     </div>
 
                 </div>
+                <hr />
                 <div className='spell-video d-flex justify-content-center'>
                     <video autoPlay muted loop className='video'>
                         <source src="https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0084/ability_0084_W1.webm" type="video/webm"></source>
                     </video>
                 </div>
+                <hr className='mb-5' />
                 <div className='align-self-center mt-2 champ-spells-overflow'>
-                    <Spells image={champion.passive.image.full} name={champion.passive.name} description={champion.passive.description} isPassive={true} />
+                    <h3 className='title'>PASSIVE:</h3>
+                    <Spells image={champion.passive.image.full} name={champion.passive.name} description={champion.passive.description} isPassive={true} letter='P' />
+                    <hr />
+                    <h3 className='title'>SPELLS:</h3>
                     {
-                        champion.spells.map((s) =>
+                        champion.spells.map((s, index) =>
 
                         (
-                            <Spells image={s.image.full} name={s.name} description={s.description} isPassive={false} key={s.id} />
+                            <div>
+                                <Spells image={s.image.full} name={s.name} description={s.description} isPassive={false} key={s.id} letter={spellsLetters[index]} />
+                                <hr />
+                            </div>
                         ))
                     }
 
@@ -49,8 +58,9 @@ const Spells = (props) => {
 
     const image = props.image;
     const name = props.name;
-    const description = props.description
-    const isPassive = props.isPassive
+    const description = props.description;
+    const isPassive = props.isPassive;
+    const letter = props.letter;
 
     return (
         <div className="mb-2 spell-content mx-3" >
@@ -62,10 +72,11 @@ const Spells = (props) => {
                 }
                 <div className='ms-2'>
                     <p className="entity-title font-weight-bold mb-1">{name}</p>
-                    <p className='spell-cooldown small mb-0'>10 / 15/ 20</p>
+                    <p className='spell-cooldown small mb-0'>10 / 15 / 20 / 25 / 30</p>
                 </div>
 
             </div>
+            <span class='spell-letter'>{letter}</span>
 
             <div className='ms-1 mt-2'>
                 <p className="spell-description m-0" dangerouslySetInnerHTML={{ __html: description }} />
