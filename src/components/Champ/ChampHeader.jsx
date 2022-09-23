3143b537ee80c846f0f607f322e49b8be57f55dd
thomas_lorthioir <tottilorthioir@gmail.com>
2022-09-23 10:30:23 -0400

    feat: (About) add animations mounted

--- src/components/Champ/ChampHeader.jsx
+++ src/components/Champ/ChampHeader.jsx
@@ -1,4 +1,5 @@
 import ChampLevel from "./ChampLevel";
+import './ChampHeader.scss'
 
 const ChampHeader = (props) => {
 
@@ -10,12 +11,12 @@ const ChampHeader = (props) => {
     return (
         <div className="champ-header  mb-5  d-flex justify-content-center">
             <div>
-                <div className="d-flex flex-column text-center">
+                <div className="d-flex flex-column text-center animation-champ-title mb-3">
                     <h1 className="title">{champion.name.toUpperCase()}</h1>
                     <h2>{champion.title}</h2>
                 </div>
                 <div className="d-flex justify-content-center mx-2  ">
-                    <div className="position-relative me-4 align-self-center">
+                    <div className="position-relative me-4 align-self-center animation-champ-square">
                         <img className="" src={`http://ddragon.leagueoflegends.com/cdn/12.16.1/img/champion/${champion.image.full}`} width='64px'></img>
                         <span className="champ-level">{level}</span>
                     </div>
@@ -23,8 +24,8 @@ const ChampHeader = (props) => {
                         <div className="d-flex flex-wrap align-items-center  ">
                             <div className=' d-flex'>
                                 {champCard.role.map((role) => (
-                                    <div key={role} className='d-flex display-entity-memo'>
-                                        <div className='text-center my-1' >
+                                    <div key={role} className='d-flex display-entity-memo animation-entity-1'>
+                                        <div className='text-center my-1 ' >
                                             <img src={require(`../../assets/Post/${role}.png`)} alt="Champion post" width="38px" height="38px"></img>
                                             <p className="title text-memo memo-description " key={role}>{role}</p>
                                         </div>
@@ -36,8 +37,8 @@ const ChampHeader = (props) => {
 
                             <div className='d-flex'>
                                 {champCard.type.map((type) => (
-                                    <div className="d-flex display-entity-memo " key={type}>
-                                        <div className='text-center my-1' >
+                                    <div className="d-flex display-entity-memo animation-entity-2 " key={type}>
+                                        <div className='text-center my-1 ' >
                                             <img src={require(`../../assets/Type/${type}.png`)} alt="Champion post" width="38px" height="38px"></img>
                                             <p className="title text-memo memo-description  " key={type}>{type}</p>
                                         </div>
@@ -47,8 +48,8 @@ const ChampHeader = (props) => {
                                 {/* <hr className="vertical-hr"></hr> */}
                             </div>
 
-                            <div className='display-entity-memo'>
-                                <div className='text-center my-1'>
+                            <div className='display-entity-memo animation-entity-3'>
+                                <div className='text-center my-1 '>
                                     <img src={require(`../../assets/Region/${champCard.world.replace(' ', '_')}.png`)} alt="Champion world" width="38px" height="38px"></img>
                                     <p className='title text-memo memo-description'>{champCard.world}</p>
                                     {/* <hr className="vertical-hr"></hr> */}
